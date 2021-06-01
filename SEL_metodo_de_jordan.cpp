dcb127048adf7621bb530e4ed6eec25e0936d9f7
Francinildo-Figueiredo <66557567+Francinildo-Figueiredo@users.noreply.github.com>
2021-05-31 22:01:08 -0300

    Ajustes finais

--- SEL_metodo_de_jordan.cpp
+++ SEL_metodo_de_jordan.cpp
@@ -14,6 +14,7 @@ int main(){
 	int n(1), ind;
 	string mat_a, mat_b;
 	
+	// Lendo as matrizes no padrão do Matlab
 	cout << "Insira a matriz dos coeficientes no padrão [1 2 3;4 5 6;7 8 9]:\n" << endl;
 	getline(cin, mat_a);
 	cout << "\nInsira a matriz igualdade no padrão [1;2;3]:\n" << endl;
@@ -50,6 +51,7 @@ int main(){
 		}
 	}
 	
+	// Convertendo os valores das strings para float e passando para a matriz B
 	mat_b.erase(mat_b.begin());
 	for(int i(0); i < n; i++){
 		B.push_back(stof(mat_b));
@@ -72,6 +74,7 @@ int main(){
 		cout << setw(5) << B[i] << endl;
 	}
 	
+	// Anexando a matriz identidade a matriz A para que possa ser realizado o cálculo da matriz inversa
 	for(int i(0); i < n; i++){
 		for(int j(0); j < n; j++){
 			if(i != j)
@@ -80,10 +83,11 @@ int main(){
 				A[i].push_back(1);
 		}
 	}
-
+	
+	// Matriz que irá receber os multiplicadores de cada linha
 	vector<vector<float>> m(A.size(), vector<float>(A.size(),0));
 	
-	//Método de eliminação de Jordan
+	//Método de eliminação de Gauss-Jordan
 	for (int j(0); j < n; j++){
 		for (int i(0); i < n; i++){
 			if(i!=j){
@@ -100,7 +104,6 @@ int main(){
 	cout << "\n\nAproximação da solução:" << endl;
 	for (int i(0); i < n; i++){
 		X[i] = B[i]/A[i][i];
-		//A(i,:) = A(i,:)/A(i,i);
 		int k(0);
 		for(int j(n); j < t; j++){
 			Inv[i][k] = A[i][j]/A[i][i];
