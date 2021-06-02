5089bc7e50f9838632f3d1c0292a3e7f5ef3b253
Francinildo-Figueiredo <66557567+Francinildo-Figueiredo@users.noreply.github.com>
2021-06-02 16:43:46 -0300

    Versão final

--- interpolacao_linear.cpp
+++ interpolacao_linear.cpp
@@ -1,3 +1,9 @@
+/* 
+O objetivo da interpolação é obter um polinômio P(X) de grau (n - 1), que passe por todos os pontos de
+um conjunto de dados {Xi, f(Xi)}. Quanto maior a quatidade n de pontos do conjunto, maior será o grau
+do polinômio.
+*/
+
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -31,19 +37,70 @@ int main(){
 	}
 	system("cls");
 	
-	vector<vector<float>> mat_x(n);
+	cout << setw(10) << "X" << setw(10) << " f(X)" << endl;
+	for(int i(0); i < n; i++){
+		cout << right << setw(10) << x[i] << right << setw(10) << y[i] << endl;
+	}
+	cout << endl;
 	
+	// Montando a matriz com as potências de Xi
+	vector<vector<float>> mat_x(n);
 	for(int i(0); i < n; i++){
 		for(int j(n-1); j >= 0; j--){
 			mat_x[i].push_back(pow(x[i],j));
 		}
 	}
 	
-	for(int i(0); i < n; i++){
-		for(int j(0); j < n; j ++){
-			cout << setw(6) << mat_x[i][j] << " ";
+	//Método de eliminação de Gauss-Jordan
+	vector<vector<float>> m(n, vector<float>(n,0));
+	for (int j(0); j < n; j++){
+		for (int i(0); i < n; i++){
+			if(i!=j){
+	    		m[i][j] = mat_x[i][j]/mat_x[j][j];
+	    		
+	    		for (int k(j); k < n; k++){
+	    			mat_x[i][k] = mat_x[i][k] - m[i][j]*mat_x[j][k];
+	    		}
+	    		y[i] = y[i] - m[i][j]*y[j];
+	    	}
 		}
-		cout << endl;
+	}
+	
+	// Determinando os coeficientes do polinômio interpolador
+	vector<float> a(n);
+	int ind(n-1);
+	cout << "Coeficientes calculados: " << endl;
+	for (int i(0); i < n; i++){
+		a[i] = y[i]/mat_x[i][i];
+		cout << "a" << ind << " = " << a[i] << endl;
+		ind--;
+	}
+	
+	cout << "\nPolinômio interpolador: " << endl;
+	cout << "\nP(X) = ";
+	ind = n-1;
+	for(int i(0); i < n; i++){
+		if(a[i] != 0 && a[i] != -0 && a[i] > 0 && i != 0)
+			cout << " +";
+		else if(a[i] < 0)
+			cout << " ";
+		if(a[i] == 0 || a[i] == -0)
+			cout << "";
+		else if(ind == 1 && a[i] != 1 && a[i] != -1)
+			cout << a[i] << "*X";
+		else if(ind == 0)
+			cout << a[i];
+		else if(ind == 1 && a[i] == 1)
+			cout << "X";
+		else if(ind == 1 && a[i] == -1)
+			cout << " -X";
+		else if(a[i] == 1)
+			cout << "X^" << ind;
+		else if(a[i] == -1)
+			cout << " -X^" << ind;
+		else
+			cout << a[i] << "*X^" << ind;
+		ind--;
 	}
 	
 	return 0;
